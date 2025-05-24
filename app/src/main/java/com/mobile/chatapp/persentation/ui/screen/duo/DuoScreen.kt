2efba287e61ad51130cb2c3ff345e0dd0d30e0bc
Sivaram.S <siva11vkavis@gmail.com>
2025-05-24 13:51:38 +0530

    chat card

--- app/src/main/java/com/mobile/chatapp/persentation/ui/screen/duo/DuoScreen.kt
+++ app/src/main/java/com/mobile/chatapp/persentation/ui/screen/duo/DuoScreen.kt
@@ -1,4 +1,5 @@
 package com.mobile.chatapp.persentation.ui.screen.duo
+
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -32,13 +33,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.mobile.chatapp.R
 import com.mobile.chatapp.persentation.navigation.appnav.AppRoutes
+import com.mobile.chatapp.persentation.ui.theme.AppTheme
 import com.mobile.chatapp.persentation.ui.theme.zohoExtraBold
 import com.mobile.chatapp.persentation.ui.theme.zohoMedium
 import com.mobile.chatapp.persentation.ui.theme.zohoRegular
@@ -46,53 +51,75 @@ import com.mobile.chatapp.persentation.ui.theme.zohoRegular
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DuoScreen(navController: NavController){
+fun DuoScreen(navController: NavController) {
 
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-        topBar = { TopAppBar(
-            title = {
-                Text(
-                    text = "AppName",
-                    style = TextStyle(
-                        fontSize = 22.sp,
-                        fontFamily = zohoExtraBold,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "AppName",
+                        style = TextStyle(
+                            fontSize = 22.sp,
+                            fontFamily = zohoExtraBold,
+                        )
                     )
-                )
-            },
-            navigationIcon = {},
-            actions = {
-                IconButton(onClick = {}) {
+                },
+                navigationIcon = {},
+                actions = {
+                    IconButton(onClick = {}) {
 
-                    Icon(painter = painterResource(R.drawable.baseline_qr_code_scanner_24), contentDescription = "")
+                        Icon(
+                            painter = painterResource(R.drawable.baseline_qr_code_scanner_24),
+                            contentDescription = ""
+                        )
 
-                }
+                    }
 
-                IconButton(onClick = {}) {
+                    IconButton(onClick = {}) {
 
-                    Icon(painter = painterResource(R.drawable.baseline_more_vert_24), contentDescription = "")
+                        Icon(
+                            painter = painterResource(R.drawable.baseline_more_vert_24),
+                            contentDescription = ""
+                        )
 
-                }
-            },
+                    }
+                },
 //            colors = TopAppBarColors(containerColor = Color.Blue, scrolledContainerColor = Color.Blue, navigationIconContentColor = Color.Blue, titleContentColor = Color.Blue, actionIconContentColor = Color.Blue),
             )
-                 },
+        },
         content = {
 
-            LazyColumn (Modifier.fillMaxSize().padding(it)){
+            LazyColumn(
+                Modifier
+                    .fillMaxSize()
+                    .padding(it)
+            ) {
                 item {
 
 
-                    Box(modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp)
-                        .height(45.dp)
-                        .background(color = Color.Gray.copy(alpha = 0.1f), shape = RoundedCornerShape(20.dp))
-                        .clickable {
-                            navController.navigate(AppRoutes.SEARCH_SCREEN)
-                        },
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 10.dp)
+                            .height(45.dp)
+                            .background(
+                                color = Color.Gray.copy(alpha = 0.1f),
+                                shape = RoundedCornerShape(20.dp)
+                            )
+                            .clickable {
+                                navController.navigate(AppRoutes.SEARCH_SCREEN)
+                            },
                         contentAlignment = Alignment.CenterStart,
-                    ){
-                        Row (Modifier.fillMaxWidth().padding(horizontal = 10.dp), verticalAlignment = Alignment.CenterVertically){
+                    ) {
+                        Row(
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 10.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
                             Icon(Icons.Outlined.Search, contentDescription = "")
                             Spacer(Modifier.width(5.dp))
                             Text(
@@ -119,27 +146,37 @@ fun DuoScreen(navController: NavController){
 }
 
 
-
 @Composable
-fun ChatItem(){
+fun ChatItem() {
 
+    Box(
+        Modifier
+            .fillMaxWidth()
+            .height(70.dp)
+            .padding(horizontal = 15.dp, vertical = 5.dp)
+    ) {
 
-    Box(Modifier.fillMaxWidth()
-        .height(55.dp)
-        .padding(horizontal = 10.dp)){
+        Row(Modifier.fillMaxSize(), verticalAlignment = Alignment.CenterVertically) {
 
-        Row (Modifier.fillMaxSize(), verticalAlignment = Alignment.CenterVertically){
-
-            Box(Modifier.size(50.dp)
-                .clip(CircleShape)
-                .background(color = MaterialTheme.colorScheme.onSurface)){
+            Box(
+                Modifier
+                    .size(60.dp)
+                    .clip(CircleShape)
+                    .background(color = MaterialTheme.colorScheme.onSurface)
+            ) {
 
             }
-
-            Spacer(Modifier.width(7.dp))
-
-            Row (Modifier.fillMaxSize(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween){
-                Column (Modifier.fillMaxHeight(),Arrangement.SpaceEvenly){
+            Row(
+                Modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(
+                    Modifier
+                        .fillMaxHeight()
+                        .padding(horizontal = 15.dp),
+                    Arrangement.SpaceEvenly
+                ) {
 
                     Text(
                         text = "Kinston",
@@ -148,39 +185,48 @@ fun ChatItem(){
                             fontFamily = zohoMedium,
                         )
                     )
-
-
-
                     Text(
                         text = "You: Ok",
                         style = TextStyle(
-                            fontSize = 15.sp,
+                            fontSize = 16.sp,
                             fontFamily = zohoRegular,
                         )
                     )
-
                 }
 
-                Column (Modifier.fillMaxHeight(),Arrangement.SpaceEvenly,Alignment.End){
+                Column(
+                    Modifier.fillMaxHeight(),
+                    horizontalAlignment = Alignment.End,
+                    verticalArrangement = Arrangement.SpaceEvenly
 
+                ) {
                     Text(
                         text = "10:45 PM",
                         style = TextStyle(
-                            fontSize = 12.sp,
+                            fontSize = 14.sp,
                             fontFamily = zohoMedium,
-                        )
+                        ),
+                        textAlign = TextAlign.Center,
+                        color = colorResource(R.color.card_text_color)
                     )
-
-
-                    Box(Modifier.clip(CircleShape).background(color = MaterialTheme.colorScheme.primaryContainer), contentAlignment = Alignment.Center){
-                        Text(
-                            modifier = Modifier.padding(5.dp),
-                            text = "4",
-                            style = TextStyle(
-                                fontSize = 10.sp,
-                                fontFamily = zohoMedium,
+                    Row(
+                    ) {
+                        Box(
+                            Modifier
+                                .size(24.dp)
+                                .clip(CircleShape)
+                                .background(color = MaterialTheme.colorScheme.primaryContainer),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                textAlign = TextAlign.Center,
+                                text = "9",
+                                style = TextStyle(
+                                    fontSize = 10.sp,
+                                    fontFamily = zohoMedium,
+                                )
                             )
-                        )
+                        }
                     }
 
                 }
@@ -193,9 +239,11 @@ fun ChatItem(){
 }
 
 
-
 @OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
 @Composable
-fun AppBar() {
-
+fun ChatPreview() {
+    AppTheme {
+        ChatItem()
+    }
 }
